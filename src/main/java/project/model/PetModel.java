8c2315d4d02ce4b3011bddc121699d764ac681c8
Kateryna <keitkat98@gmail.com>
2024-08-14 17:38:59 +0300

    start of model creation

---
+++ src/main/java/project/model/PetModel.java
@@ -0,0 +1,9 @@
+package project.model;
+
+import java.time.LocalDateTime;
+
+public abstract class PetModel {
+    private String petName;
+    private LocalDateTime petAdoptDate;
+    private String petDescription;
+}
